1ccd76aa9ff3caf573acd95f535c6442b4090a3e
Rohan Tapulli <74230154+RohanTapulli1311@users.noreply.github.com>
2022-04-20 22:01:08 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,7 +1,3 @@
 #display the contents of the file
 print("this is a new file")
-print("this is edited new line")
-print("edited again lol")
-print("adding a line to just addmy contribution for the day!")
-print("same reason as above, pls dont mind!")
-print("same reason as above, pls dont mind!")
+
